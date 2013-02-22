81c17736f67a7992a58bb86279fc24cfa4fdf04a
raffi287 <>
2013-02-22 20:12:42 +0100

    -Blockhead geaddet

--- src/com/tauncraft/tauncraftservermanager/commands/FunCommands.java
+++ src/com/tauncraft/tauncraftservermanager/commands/FunCommands.java
@@ -9,6 +9,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 /**
@@ -17,29 +18,51 @@ import org.bukkit.plugin.Plugin;
  * Raffael
  */
 public class FunCommands implements CommandExecutor {
-    
-    
+
     private Plugin plugin;
 
     public FunCommands(TauncraftServerManager plugin) {
         this.plugin = plugin;
     }
-    
+
     /**
-     * Beim eingeben eines Command
+     * Beim
+     * eingeben
+     * eines
+     * Command
      *
-     * @param sender sender des Commands
-     * @param command Command
-     * @param label Name des Commands
-     * @param args Parameter des Commands
+     * @param
+     * sender
+     * sender
+     * des
+     * Commands
+     * @param
+     * command
+     * Command
+     * @param
+     * label
+     * Name
+     * des
+     * Commands
+     * @param
+     * args
+     * Parameter
+     * des
+     * Commands
      * @return
      */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender.hasPermission("taunsm.fun." + label) || sender.hasPermission("taunsm.fun.*") ||sender.hasPermission("taunsm.*") || sender.isOp()) {
+        if (sender.hasPermission("taunsm.fun." + label) || sender.hasPermission("taunsm.fun.*") || sender.hasPermission("taunsm.*") || sender.isOp()) {
             if (sender instanceof Player) {
                 switch (label) {
-                    case "command":
+                    case "blockhead":
+                        blockhead((Player) sender, args);
+                        break;
+                    case "command1":
+                        //method((Player) sender, args);
+                        break;
+                    case "command2":
                         //method((Player) sender, args);
                         break;
                     default:
@@ -52,5 +75,20 @@ public class FunCommands implements CommandExecutor {
         }
         //Ausgabe: "Du hast nicht die nötigen Permissions"
         return false;
+    }
+
+    public void blockhead(Player spieler, String[] args) {
+        ItemStack i;
+        if (args.length >= 1) {
+            String[] sa = args[0].split(":");
+            i = new ItemStack(Integer.parseInt(sa[0]));
+            if (sa.length >= 2) {
+                i.setDurability(Short.parseShort(sa[1]));
+            }
+
+        } else {
+            i = spieler.getItemInHand();
+        }
+        spieler.getInventory().setHelmet(i);
     }
 }
