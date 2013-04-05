ebd8fd9d806fc2996f740d90f9ee21adb64550ed
Terradominik <terradominik@tauncraft.com>
2013-04-05 11:13:48 +0200

    Logger removed

--- src/com/tauncraft/tauncraftservermanager/Ports.java
+++ src/com/tauncraft/tauncraftservermanager/Ports.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.Location;
 import org.bukkit.World;
 
