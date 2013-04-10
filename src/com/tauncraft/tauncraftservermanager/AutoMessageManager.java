0df59da291c48d9a6602ad844f09872fbef6de4c
Terradominik <terradominik@tauncraft.com>
2013-04-10 13:59:15 +0200

    AutoMessanger - Kommentar geadded :)

--- src/com/tauncraft/tauncraftservermanager/AutoMessageManager.java
+++ src/com/tauncraft/tauncraftservermanager/AutoMessageManager.java
@@ -36,6 +36,9 @@ public class AutoMessageManager {
         this.broadcastMessages();
     }
     
+    /**
+     * Sendet im Intervall "period" eine Broadcast Message
+     */
     private void broadcastMessages() {
         new BukkitRunnable() {
             int rand = new Random().nextInt(messages.size());
