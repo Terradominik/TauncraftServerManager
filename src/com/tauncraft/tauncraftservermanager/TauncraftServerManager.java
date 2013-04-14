3bb66b221dc52f92848a717bcb2325b2ab394545
raffi287 <>
2013-04-14 10:53:16 +0200

    effect zu effekt gemacht das /effect von bukkit nicht geblockt wird

--- src/com/tauncraft/tauncraftservermanager/TauncraftServerManager.java
+++ src/com/tauncraft/tauncraftservermanager/TauncraftServerManager.java
@@ -105,7 +105,7 @@ public class TauncraftServerManager extends JavaPlugin {
         
         //Fun Commands
         this.getCommand("blockhead").setExecutor(fc);
-        this.getCommand("effect").setExecutor(fc);
+        this.getCommand("effekt").setExecutor(fc);
         this.getCommand("head").setExecutor(fc);
         this.getCommand("name").setExecutor(fc);
         
